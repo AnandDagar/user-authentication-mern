84c1af4ab814da79831071f673fde2fa85834f1b
AnandDagar <ananddagar111@gmail.com>
2024-12-09 07:24:34 +0530

    complete- user-auth-dasboared-page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,17 +7,18 @@ import Profile from "./components/Profile";
 import Header from "./components/Header";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import ProtectedRoute from "./components/ProtectedRoute";
+// import ProtectedRoute from "./components/ProtectedRoute";
 const App = () => {
   return (
     <>
       <Router>
         <Header />
         <Routes>
-          <Route element={<ProtectedRoute />}>
+          {/* <Route element={<ProtectedRoute />}>
             <Route path="/profile" element={<Profile />} />
-          </Route>
+          </Route> */}
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Registration />} />
         </Routes>
